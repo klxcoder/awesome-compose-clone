0f7576f9ba9f2a7356fe9adc06f69a407924632c
klxcoder <klxcoder@gmail.com>
2024-12-07 21:10:46 +0700

    route / return from database

--- react-express-mysql/backend/src/server.js
+++ react-express-mysql/backend/src/server.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const morgan = require("morgan");
 
+const database = require("./database");
+
 const app = express();
 
 app.use(morgan("common"));
 
-app.get("/", function (req, res) {
-  res.send("Hello World from server");
+app.get("/", function (req, res, next) {
+  database.raw('select VERSION() version')
+    .then(([rows, columns]) => rows[0])
+    .then((row) => res.json({ message: `Hello from MySQL ${row.version}` }))
+    .catch(next);
 });
 
 app.get("/healthz", function (req, res) {
